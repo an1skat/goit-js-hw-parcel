10481c8bce135099b7798a0225709360f7e5a4c0
an1skat <156462349+an1skat@users.noreply.github.com>
2025-04-22 19:40:29 +0300

    Updates

---
+++ goit-js-hw-parcel.6313c220.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("input"),t=document.querySelector("button"),u=document.querySelector("h1");t.addEventListener("click",()=>{let t=e.value.trim();u.textContent=t?`\u{41F}\u{440}\u{438}\u{432}\u{456}\u{442} ${t}!`:"Введи ім’я!"});
+//# sourceMappingURL=goit-js-hw-parcel.6313c220.js.map
